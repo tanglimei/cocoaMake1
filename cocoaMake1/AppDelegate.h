2e5b9437478e1c2989ecaff2667a53fc77a6f7ff
GITang <562630424@qq.com>
2015-12-16 22:03:56 +0800

    Initial Commit

---
+++ cocoaMake1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  cocoaMake1
+//
+//  Created by tlm on 15/12/16.
+//  Copyright © 2015年 Genus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ cocoaMake1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  cocoaMake1
+//
+//  Created by tlm on 15/12/16.
+//  Copyright © 2015年 Genus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
